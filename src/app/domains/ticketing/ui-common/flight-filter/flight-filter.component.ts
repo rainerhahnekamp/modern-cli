4d695ac868a35d9b8723a9f7373908273bd81820
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-10-04 01:14:08 +0200

    not working at the moment.

--- src/app/domains/ticketing/ui-common/flight-filter/flight-filter.component.ts
+++ src/app/domains/ticketing/ui-common/flight-filter/flight-filter.component.ts
@@ -1,21 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, effect, inject } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  effect,
+  inject,
+  untracked,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { FlightFilter } from '../../data';
 import { FlightFilterStore } from './flight-filter.store';
 
 @Component({
   selector: 'app-flight-filter',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './flight-filter.component.html',
   styleUrls: ['./flight-filter.component.css'],
-  providers: [
-    FlightFilterStore
-  ]
+  providers: [FlightFilterStore],
 })
 export class FlightFilterComponent {
   @Input() set filter(filter: FlightFilter) {
@@ -27,25 +35,35 @@ export class FlightFilterComponent {
   inputFilterForm = inject(FormBuilder).nonNullable.group({
     from: ['', [Validators.required]],
     to: ['', [Validators.required]],
-    urgent: [false]
+    urgent: [false],
   });
 
-  selectedFilterControl = new FormControl(this.inputFilterForm.getRawValue(), { nonNullable: true });
+  selectedFilterControl = new FormControl(this.inputFilterForm.getRawValue(), {
+    nonNullable: true,
+  });
 
   protected localStore = inject(FlightFilterStore);
 
   constructor() {
-    this.localStore.initInputFilterUpdate(this.inputFilterForm.valueChanges);
-    this.localStore.initSelectedFilterUpdate(this.selectedFilterControl.valueChanges);
-    effect(() => this.inputFilterForm.patchValue(this.localStore.selectedFilter()), {
-      allowSignalWrites: true
+    this.inputFilterForm.valueChanges.subscribe((value) => {
+      this.localStore.updateInputFilter(value as FlightFilter);
     });
-    effect(() => this.selectedFilterControl.setValue(this.localStore.latestFilter()), {
-      allowSignalWrites: true
-    });
+
     effect(() => {
-      const latestFilter = this.localStore.latestFilter();
-      latestFilter && this.searchTrigger.emit(latestFilter);
+      const selectedFilter = this.localStore.selectedFilter();
+      console.log(selectedFilter);
+      // this.inputFilterForm.patchValue(this.localStore.selectedFilter());
     });
+    // this.localStore.initInputFilterUpdate(this.inputFilterForm.valueChanges);
+    // this.localStore.initSelectedFilterUpdate(
+    //   this.selectedFilterControl.valueChanges
+    // );
+    // effect(() =>
+    //   this.selectedFilterControl.setValue(this.localStore.latestFilter())
+    // );
+    // effect(() => {
+    //   const latestFilter = this.localStore.latestFilter();
+    //   latestFilter && this.searchTrigger.emit(latestFilter);
+    // });
   }
 }
